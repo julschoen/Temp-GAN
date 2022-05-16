2b5dbc11c5ea6e72d9f5636cdbb1468c3eea4e4f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-16 12:48:15 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -32,14 +32,14 @@ def main():
 	parser.add_argument('-p', '--path', type=str, default='')
 	params = parser.parse_args()
 
-	dirs = sorted([f for f in os.listdir(params.path)],
+	dirs = sorted([f for f in os.listdir(params.path) if os.path.isdir(os.path.join(params.path, f))],
 		key=lambda x: int(x))
 	print(dirs)
 	for d in dirs:
-		subdirs = sorted([f for f in os.listdir(d) if os.path.isdir(f)],
+		subdirs = sorted([f for f in os.listdir(os.path.join(params.path, d)) if os.path.isdir(os.path.join(params.path,d,f))],
 				key=lambda x: int(x))
 		for sub in subdirs:
-			files = sorted([f for f in os.listdir(os.path.join(d,sub)) if f.startswith('cbct')],
+			files = sorted([f for f in os.listdir(os.path.join(params.path,d,sub)) if f.startswith('cbct')],
 							key=lambda x: int(x[4:5]) if x[4:6].endswith('_') else int(x[4:6]))
 			if len(files)<2:
 				print(d, sub)
