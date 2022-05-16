e706fcc641c65b864a2562514c1f3736882e28dc
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-16 11:58:09 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,7 @@ def main():
 	parser.add_argument('-p', '--path', type=str, default='')
 	params = parser.parse_args()
 	files = sorted([f for f in os.listdir(params.path) if f.startswith('cbct')],
-		key=lambda x: x[4:5] if x[4:6].endswith('_') else x[4:6])
+		key=lambda x: int(x[4:5]) if x[4:6].endswith('_') else int(x[4:6]))
 	print(files)
 	for f in files:
 		img = nib.load(os.path.join(params.path,f))
