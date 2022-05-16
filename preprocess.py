10ceca8d49e453dbcfad6ca3f6a1680050ec3211
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-16 13:01:55 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,7 @@ import argparse
 import torch
 from torch.nn.functional import interpolate
 
-def process(path):
-	files = sorted([f for f in os.listdir(path) if f.startswith('cbct')],
-		key=lambda x: int(x[4:5]) if x[4:6].endswith('_') else int(x[4:6]))
+def process(path, files):
 	ims = None
 	for f in files:
 		img = nib.load(os.path.join(path,f))
@@ -25,15 +23,30 @@ def process(path):
 		else:
 			ims = img
 
-		return ims
+		return ims.numpy()
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', '--path', type=str, default='')
+	parser.add_argument('-d', '--data_path', type=str, default='')
+	parser.add_argument('-s', '--save_path', type=str, default='data')
 	params = parser.parse_args()
 
-	files = sorted([f for f in os.listdir(params.path) if os.path.isdir(f)],
+	dirs = sorted([f for f in os.listdir(params.path) if os.path.isdir(os.path.join(params.path, f))],
 		key=lambda x: int(x))
+
+	for d in dirs:
+		subdirs = sorted([f for f in os.listdir(os.path.join(params.data_path, d)) 
+									if os.path.isdir(os.path.join(params.data_path,d,f))],
+				key=lambda x: int(x))
+		for sub in subdirs:
+			files = sorted([f for f in os.listdir(os.path.join(params.data_path,d,sub)) if f.startswith('cbct')],
+							key=lambda x: int(x[4:5]) if x[4:6].endswith('_') else int(x[4:6]))
+			if len(files)>1:
+				s.makedirs(os.path.join(params.save_path, d), exist_ok=True)
+				ims = preprocess(os.path.join(params.data_path, d, sub), files)
+				np.savez_compressed(f'{sub}.npz', x=ims)
+
+				
 
 	ims = process(files[-1])
 	print(ims.shape)
