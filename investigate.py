e7a4e7e9989e9c8b38787a88440bad4d59a676f5
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-18 13:06:44 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -11,14 +11,12 @@ def main():
 	for d in dirs:
 		if os.path.isdir(os.path.join(path, d)):
 			new_dirs.append(d)
-	print(new_dirs)
+	dirs = np.array(new_dirs)
 
 	data = None
 	for d in dirs:
 		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
 					key=lambda x: int(x[:-4]))
-		
-		print(d,files)
 		for f in files:
 			x = np.load(os.path.join(path,d,f))['x']
 			if x.shape[0] < 3:
@@ -29,6 +27,14 @@ def main():
 					key=lambda x: int(x[:-4]))
 		if len(files) < 1:
 			os.remove(os.path.join(path, d))
+
+	new_dirs = []
+	for d in dirs:
+		if os.path.isdir(os.path.join(path, d)):
+			new_dirs.append(d)
+	dirs = np.array(new_dirs)
+	print(dirs.shape)
+	np.savez_compressed(os.path.join(path, 'train_pat.npz'), x=dirs)
 
 if __name__ == '__main__':
 	main()
