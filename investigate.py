5a5f34cd1585dde563364ba8c9ea226ffcf7e8e3
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-18 13:35:35 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -6,35 +6,27 @@ from torch.nn.functional import interpolate
 
 def main():
 	path = 'data'
-	dirs = np.load(os.path.join(path, 'test_pat.npz'))['x']
-	new_dirs = []
-	for d in dirs:
-		if os.path.isdir(os.path.join(path, d)):
-			new_dirs.append(d)
-	dirs = np.array(new_dirs)
-
-	data = None
-	for d in dirs:
-		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
-					key=lambda x: int(x[:-4]))
-		for f in files:
-			x = np.load(os.path.join(path,d,f))['x']
-			if x.shape[0] < 3:
-				os.remove(os.path.join(path, d, f))
-
-	for d in dirs:
-		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
-					key=lambda x: int(x[:-4]))
-		if len(files) < 1:
-			os.rmdir(os.path.join(path, d))
+	dirs = np.load(os.path.join(path, 'train_pat.npz'))['x']
 
 	new_dirs = []
 	for d in dirs:
-		if os.path.isdir(os.path.join(path, d)):
-			new_dirs.append(d)
+		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
+					key=lambda x: int(x[:-4]))
+		new_dirs.append(d)
+		if len(files) > 1:
+			for i, f in enumerate(files):
+				x = np.load(os.path.join(path,d,f))['x']
+				if i == 0:
+					np.savez_compressed(os.path.join(path, d, '0.npz') x=x)
+				else:
+					new_path = os.path.join(path, d+f'_{i}')
+					os.makedirs(new_path, exist_ok=True)
+					np.savez_compressed(os.path.join(new_path, '0.npz') x=x)
+					new_dirs.append(new_path)
+
 	dirs = np.array(new_dirs)
 	print(dirs.shape)
-	np.savez_compressed(os.path.join(path, 'test_pat.npz'), x=dirs)
+	np.savez_compressed(os.path.join(path, 'train_pat.npz'), x=dirs)
 
 if __name__ == '__main__':
 	main()
