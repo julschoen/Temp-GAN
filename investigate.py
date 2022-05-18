12c274334aae3902a9347f2c665a50d0c2c88f8e
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-18 13:36:28 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -6,7 +6,7 @@ from torch.nn.functional import interpolate
 
 def main():
 	path = 'data'
-	dirs = np.load(os.path.join(path, 'train_pat.npz'))['x']
+	dirs = np.load(os.path.join(path, 'test_pat.npz'))['x']
 
 	new_dirs = []
 	for d in dirs:
@@ -26,7 +26,7 @@ def main():
 
 	dirs = np.array(new_dirs)
 	print(dirs.shape)
-	np.savez_compressed(os.path.join(path, 'train_pat.npz'), x=dirs)
+	np.savez_compressed(os.path.join(path, 'test_pat.npz'), x=dirs)
 
 if __name__ == '__main__':
 	main()
