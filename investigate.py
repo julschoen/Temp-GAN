731eebe93c81d67eed405b8ca7c2e7b311aa4b96
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-18 12:31:00 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -1,29 +1,23 @@
 import numpy as np
 import os
-import nibabel as nib
 import argparse
 import torch
 from torch.nn.functional import interpolate
 
 def main():
 	path = 'data'
-	dirs = np.load(os.path.join(path, 'test_pat.npz'))['x']
+	dirs = np.load(os.path.join(path, 'train_pat.npz'))['x']
 
 	data = None
 	for d in dirs:
 		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
 					key=lambda x: int(x[:-4]))
-
+		print(d,files)
+		shapes = []
 		for f in files:
 			x = np.load(os.path.join(path,d,f))['x']
-			
-			if data is None:
-				data = x
-			else:
-				data = np.concatenate((data, x))
-
-	print(data.shape)
-	np.savez_compressed('test_cbct.npz', x=data)
+			shapes.append(x.shape)
+		print(shapes)
 
 if __name__ == '__main__':
 	main()
