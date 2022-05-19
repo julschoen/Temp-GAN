b3d596e2566dee08ead59212cf489a42ebc3f806
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-19 14:19:57 +0200

    Investigate data

--- investigate.py
+++ investigate.py
@@ -3,30 +3,26 @@ import os
 import argparse
 import torch
 from torch.nn.functional import interpolate
+import nibabel as nib
 
 def main():
-	path = 'data'
-	dirs = np.load(os.path.join(path, 'test_pat.npz'))['x']
+	path = '../../../media/7tb_encrypted/julians_project/anon_images_updated'
 
-	new_dirs = []
+	dirs = sorted([f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))],
+		key=lambda x: int(x))
 	for d in dirs:
-		files = sorted([f for f in os.listdir(os.path.join(path, d)) if f.endswith('.npz')],
-					key=lambda x: int(x[:-4]))
-		new_dirs.append(d)
-		if len(files) > 1:
-			for i, f in enumerate(files):
-				x = np.load(os.path.join(path,d,f))['x']
-				if i == 0:
-					np.savez_compressed(os.path.join(path, d, '0.npz'), x=x)
-				else:
-					new_path = os.path.join(path, d+f'_{i}')
-					os.makedirs(new_path, exist_ok=True)
-					np.savez_compressed(os.path.join(new_path, '0.npz'), x=x)
-					new_dirs.append(new_path)
-
-	dirs = np.array(new_dirs)
-	print(dirs.shape)
-	np.savez_compressed(os.path.join(path, 'test_pat.npz'), x=dirs)
+		subdirs = sorted([f for f in os.listdir(os.path.join(path, d)) 
+									if os.path.isdir(os.path.join(path,d,f))],
+				key=lambda x: int(x))
+		for sub in subdirs:
+			files = sorted([f for f in os.listdir(os.path.join(path,d,sub)) if f.startswith('cbct')],
+							key=lambda x: int(x[4:5]) if x[4:6].endswith('_') else int(x[4:6]))
+			if len(files)>1:
+				for f in files:
+					if not f.endswith('.gz'):
+						continue
+					img = nib.load(os.path.join(path,f))
+					print(img.header)
 
 if __name__ == '__main__':
 	main()
