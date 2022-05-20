ce8b5883fb9b51b765bf1c0159db26663c23b965
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-20 16:20:12 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -276,7 +276,7 @@ class Trainer(object):
         for p in self.imG.parameters():
             p.requires_grad = False
 
-        return errImG.item(), errTempG.item()
+        return errImG.item(), 0
 
     def train(self):
         step_done = self.start_from_checkpoint()
