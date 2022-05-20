73f5ca147c647c3d3eb6f67f5bd16c8be68b3f54
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-20 12:20:17 +0200

    Update investigate.py

--- investigate.py
+++ investigate.py
@@ -6,8 +6,29 @@ from torch.nn.functional import interpolate
 import nibabel as nib
 
 def main():
-	print(np.load('data/test_pat.npz')['x'])
-	print(np.load('data/train_pat.npz')['x'])
+	test = np.load('data/test_pat.npz')['x']
+	train = np.load('data/train_pat.npz')['x']
+
+	new_test = []
+	new_train = []
+
+	for t in test:
+		if t.startswith('data_'):
+			new_test.append(t[5:])
+		else:
+			new_test.append(t)
+
+	for t in train:
+		if t.startswith('data_'):
+			new_train.append(t[5:])
+		else:
+			new_train.append(t)
+
+	print(new_train)
+	print(new_test)
+
+	np.savez_compressed('data/test_pat.npz', x=np.array(new_test))
+	np.savez_compressed('data/train_pat.npz', x=np.array(new_train))
 
 if __name__ == '__main__':
 	main()
