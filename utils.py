2ec26f947c2ec1ad5ac81a2819beecea7fc8a1f3
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-23 12:50:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,12 +14,12 @@ class TripletLoss(torch.nn.Module):
     
     def forward(self, pred, inds):
       inds = inds - inds[:,0].repeat(3).reshape(3,-1).T
-      print(torch.isnan(pred).sum())
       mid = inds[:,1] == inds[:,2]/2
       low = inds[:,1] < inds[:,2]/2
       high = inds[:,1] > inds[:,2]/2
       pred = pred.unsqueeze(-1)
       loss = self.dist(pred[:,1], pred[:,0]) + self.dist(pred[:,1], pred[:,2]) - 2*self.dist(pred[:,2], pred[:,0])
+      print(torch.isnan(loss).sum())
       loss[low] = self.dist(pred[low,1], pred[low,0]) - self.dist(pred[low,1], pred[low,2])
       loss[high] = self.dist(pred[high,1], pred[high,2]) - self.dist(pred[high,1], pred[high,0])
       return torch.mean(loss)
