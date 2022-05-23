a2138ba521c890ea5fd6a0bdc1324cfa0bc2c22a
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-23 11:25:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ class TripletLoss(torch.nn.Module):
     def forward(self, pred, inds):
       inds = inds - inds[:,0].repeat(3).reshape(3,-1).T
       mid = inds[:,1] < inds[:,2]/2
-      print(mid.shape)
+      print(pred)
       losses = -torch.log(torch.exp(torch.cdist(pred[:,1], pred[:,0]))/
         (torch.exp(torch.cdist(pred[:,1], pred[:,2]))+torch.exp(torch.cdist(pred[:,1], pred[:,0]))))
 
