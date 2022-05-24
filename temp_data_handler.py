42510a66d4b673c9f3e37b084d50c6529e1ab64e
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-24 14:21:06 +0200

    Update temp_data_handler.py

--- temp_data_handler.py
+++ temp_data_handler.py
@@ -7,11 +7,11 @@ import os
 class DATA(Dataset):
   def __init__(self, path): 
     self.path = path
-    self.files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isdir(os.path.join(path,f))]
+    self.files = np.load(path)['x']
     self.len = len(self.files)
 
   def __getitem__(self, index):
-      pat = [os.path.join(self.files[index], f) for f in os.listdir(self.files[index]) if f.endswith('npz')][0]
+      pat = [os.path.join(self.path, self.files[index], f) for f in os.listdir(self.files[index]) if f.endswith('npz')][0]
       x = np.load(pat)['x']
       ind = np.random.choice(x.shape[0], 3)
       xs = x[ind]
