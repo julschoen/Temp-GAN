9d2fc5d1646370c6fbc997e4824ac0e089e9055a
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-24 14:22:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 	parser.add_argument('--iterD', type=int, default=1, help='Number of D iters per iter')
 	parser.add_argument('--lrG', type=float, default=1e-4, help='Learning rate G')
 	parser.add_argument('--lrD', type=float, default=1e-4, help='Learning rate D')
-	parser.add_argument('--data_path', type=str, default='data',help='Path to data.')
+	parser.add_argument('--data_path', type=str, default='data/train_pat.npz',help='Path to data.')
 	parser.add_argument('--ngpu', type=int, default=2, help='Number of GPUs')
 	parser.add_argument('--steps_per_log', type=int, default=10, help='Output Iterations')
 	parser.add_argument('--steps_per_img_log', type=int, default=50, help='Image Save Iterations')
