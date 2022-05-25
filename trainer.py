82caf49ee28939696db5d4d1fdd69935517d3375
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-25 15:16:03 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -403,7 +403,7 @@ class Trainer(object):
             self.imD_losses.append((errImD_real, errImD_fake))
             self.tempD_losses.append((errTempD_real, errTempD_fake))
             self.Rec_losses.append(err_rec)
-
+            print(real.shape, fake.shape)
             self.log(i, fake, real[:,0])
             if i%100 == 0 and i>0:
                 self.fid_epoch.append(np.array(self.fid).mean())
