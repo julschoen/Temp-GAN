bf5d12ad205a6e844aa9191abe860e0a3facfd1b
julschoen <31760205+julschoen@users.noreply.github.com>
2022-05-25 13:42:51 +0200

    Update temp_gen.py

--- temp_gen.py
+++ temp_gen.py
@@ -5,8 +5,10 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.dim_z = params.z_size
         self.ngpu = params.ngpu
-        self.lin = nn.Linear(in_features=self.dim_z, out_features=self.dim_z)
+        self.lin = nn.Linear(in_features=512, out_features=1)
+        self.pad = nn.ConstantPad1d((0,self.dim_z-1), 0)
 
     def forward(self, input):
         output = self.lin(input)
+        output = input + self.pad(output)
         return output
