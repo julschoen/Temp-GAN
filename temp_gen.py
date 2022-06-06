85b11bdeecd791eaab9cfdf6517bdfe400bdf398
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-06 17:22:34 +0200

    Update temp_gen.py

--- temp_gen.py
+++ temp_gen.py
@@ -8,6 +8,7 @@ class Generator(nn.Module):
         self.ngpu = params.ngpu
         self.lin = nn.Linear(in_features=self.dim_z, out_features=self.dim_z)
         self.lin.weight.data = 0.01 * torch.randn_like(self.lin.weight.data)
+        self.tanh = nn.Tanh()
 
     def forward(self, input):
-        return self.lin(input)
+        return self.tanh(self.lin(input))
