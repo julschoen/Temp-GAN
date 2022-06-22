efdb675d57126b6c9644a5e5244efe62f4131083
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-22 12:01:45 +0200

    Update eval_temp.py

--- eval_temp.py
+++ eval_temp.py
@@ -40,9 +40,9 @@ def eval(params):
 					z = torch.randn(params.batch_size, imG.dim_z, dtype=torch.float, device=params.device)
 				alpha = torch.rand(5).sort()[0]*10
 				im = imG(z).reshape(-1,1,128,128,128)
-				im1 = imG(z+direction*alpha[0]).reshape(-1,1,128,128,128)
-				im2 = imG(z+direction*alpha[1]).reshape(-1,1,128,128,128)
-				ims = torch.concat((im, im1, im2), dim=1)
+				for a in alpha:
+					im1 = imG(z+direction*a).reshape(-1,1,128,128,128)
+					im = torch.concat((im, im1), dim=1)
 		
 		np.savez_compressed(os.path.join(params.log_dir,f'{model_path}_temp.npz'),x=ims.detach().cpu().numpy())
 
