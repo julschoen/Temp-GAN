0d17fcc1de9b52dcc51421729f61ce7f5fc528da
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-29 15:09:24 +0200

    Update temp_disc_alt.py

--- temp_disc_alt.py
+++ temp_disc_alt.py
@@ -7,37 +7,22 @@ from utils import conv3x3x3, downsample_basic_block, BasicBlock, Bottleneck, Con
 
 
 class ResNet(nn.Module):
-    def __init__(self,
-                 block,
-                 layers,
-                 num_seg_classes=1,
-                 shortcut_type='B',
-                 no_cuda = False):
+    def __init__(self, block, layers):
         self.inplanes = 64
-        self.no_cuda = no_cuda
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv3d(
-            3,
-            64,
-            kernel_size=7,
-            stride=(2, 2, 2),
-            padding=(3, 3, 3),
-            bias=False)
         #self.conv1 = Conv3_1d(1, 64, kernel_size=(3,7,7,7), stride=(1, 2, 2, 2), padding=(0, 3, 3, 3), bias=False)
-            
-        self.bn1 = nn.BatchNorm3d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0], shortcut_type)
-        self.layer2 = self._make_layer(
-            block, 128, layers[1], shortcut_type, stride=2)
-        self.layer3 = self._make_layer(
-            block, 256, layers[2], shortcut_type, stride=1, dilation=2)
-        self.layer4 = self._make_layer(
-            block, 512, layers[3], shortcut_type, stride=1, dilation=4)
-
-        self.head = nn.Linear(2048, 1)
-
+        self.extractor = nn.Sequential(
+            nn.Conv3d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2, padding=1),
+            self._make_layer(block, 64, layers[0]),
+            self._make_layer(block, 128, layers[1], stride=2),
+            self._make_layer(block, 256, layers[2], stride=1, dilation=2),
+            self._make_layer(block, 512, layers[3], expansion=1, stride=1, dilation=4)
+        ) 
+        
+        self.head = nn.Linear(512*3, 1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv3d):
@@ -46,43 +31,33 @@ class ResNet(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
-    def _make_layer(self, block, planes, blocks, shortcut_type, stride=1, dilation=1):
+    def _make_layer(self, block, planes, blocks, expansion=None, stride=1, dilation=1):
+        if expansion is None:
+            expansion = block.expansion
         downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            if shortcut_type == 'A':
-                downsample = partial(
-                    downsample_basic_block,
-                    planes=planes * block.expansion,
+        if stride != 1 or self.inplanes != planes * expansion:
+            downsample = nn.Sequential(
+                nn.Conv3d(
+                    self.inplanes,
+                    planes * expansion,
+                    kernel_size=1,
                     stride=stride,
-                    no_cuda=self.no_cuda)
-            else:
-                downsample = nn.Sequential(
-                    nn.Conv3d(
-                        self.inplanes,
-                        planes * block.expansion,
-                        kernel_size=1,
-                        stride=stride,
-                        bias=False), nn.BatchNorm3d(planes * block.expansion))
+                    bias=False), nn.BatchNorm3d(planes * expansion))
 
         layers = []
         layers.append(block(self.inplanes, planes, stride=stride, dilation=dilation, downsample=downsample))
-        self.inplanes = planes * block.expansion
+        self.inplanes = planes * expansion
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes, dilation=dilation))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = torch.sum(x, [2, 3, 4])
-        return self.head(x)
+        h1 = torch.sum(self.extractor(x[0]), [2, 3, 4])
+        h2 = torch.sum(self.extractor(x[1]), [2, 3, 4])
+        h3 = torch.sum(self.extractor(x[2]), [2, 3, 4])
+        h = torch.concat((h1,h2,h3)) 
+        return self.head(h)
 
 
 
