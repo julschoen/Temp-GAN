a0dd901975271cd8e7b3edff30cd563ca6e59124
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-05 13:27:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -53,23 +53,24 @@ class Attention(nn.Module):
     super(Attention, self).__init__()
     # Channel multiplier
     self.ch = ch
-    self.red = 8
-    self.f = snconv3d(self.ch, self.ch // self.red, kernel_size=1, padding=0, bias=False)
-    self.g = snconv3d(self.ch, self.ch // self.red, kernel_size=1, padding=0, bias=False)
-    self.h = snconv3d(self.ch, self.ch // 2, kernel_size=1, padding=0, bias=False)
-    self.o = snconv3d(self.ch // 2, self.ch, kernel_size=1, padding=0, bias=False)
+    self.ch_ = self.ch//8
+
+    self.f = snconv3d(self.ch, self.ch_, kernel_size=1, padding=0, bias=False)
+    self.g = snconv3d(self.ch, self.ch_, kernel_size=1, padding=0, bias=False)
+    self.h = snconv3d(self.ch, self.ch_, kernel_size=1, padding=0, bias=False)
+    self.v = snconv3d(self.ch_, self.ch, kernel_size=1, padding=0, bias=False)
     self.gamma = P(torch.tensor(0.), requires_grad=True)
   def forward(self, x, y=None):
     # Apply convs
     f = self.f(x)
     g = F.max_pool3d(self.g(x), [2,2,2], stride=2)
-    f = f.view(-1, self.ch // self.red, x.shape[2] * x.shape[3] * x.shape[4])
-    g = g.view(-1, self.ch // self.red, x.shape[2] * x.shape[3] * x.shape[4]//8)
+    f = f.view(-1, self.ch_, x.shape[2] * x.shape[3] * x.shape[4])
+    g = g.view(-1, self.ch_, x.shape[2] * x.shape[3] * x.shape[4]//8)
     beta = F.softmax(torch.bmm(f.permute(0,2,1), g), -1)
 
     h = F.max_pool3d(self.h(x), [2,2,2], stride=2)  
-    h = h.view(-1, self.ch // 2, x.shape[2] * x.shape[3] * x.shape[4]//8)
-    o = self.o(torch.bmm(h, beta.permute(0,2,1)).view(-1, self.ch // 2, x.shape[2], x.shape[3], x.shape[4]))
+    h = h.view(-1, self.ch_, x.shape[2] * x.shape[3] * x.shape[4]//8)
+    o = self.v(torch.bmm(h, beta.permute(0,2,1)).view(-1, self.ch_, x.shape[2], x.shape[3], x.shape[4]))
     return self.gamma * o + x
 
 class GBlock(nn.Module):
