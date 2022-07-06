798501feb731b9190de34959ab6c5d0da0256ad1
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-06 10:24:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--niters', type=int, default=10000, help='Number of training iterations')
 	parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
-	parser.add_argument('--z_size', type=int, default=512, help='Latent space dimension')
+	parser.add_argument('--z_size', type=int, default=256, help='Latent space dimension')
 	parser.add_argument('--filterG', type=int, default=64, help='Number of filters G')
 	parser.add_argument('--filterD', type=int, default=64, help='Number of filters D')
 	parser.add_argument('--iterD', type=int, default=2, help='Number of D iters per iter')
