60b2fabf251cc757e76690668b1f3c37d4fc5239
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-07 14:26:50 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.nn.utils.spectral_norm as SpectralNorm
+from torch.nn.utils.parametrizations import spectral_norm as SpectralNorm
 import functools
 from torch.nn import Parameter as P
 
@@ -51,7 +51,6 @@ def snlinear(in_features, out_features):
 class Attention(nn.Module):
   def __init__(self, ch):
     super(Attention, self).__init__()
-    # Channel multiplier
     self.ch = ch
     self.ch_ = self.ch//8
 
@@ -61,7 +60,6 @@ class Attention(nn.Module):
     self.v = snconv3d(self.ch_, self.ch, kernel_size=1, padding=0, bias=False)
     self.gamma = P(torch.tensor(0.), requires_grad=True)
   def forward(self, x, y=None):
-    # Apply convs
     f = self.f(x)
     g = F.max_pool3d(self.g(x), [2,2,2], stride=2)
     f = f.view(-1, self.ch_, x.shape[2] * x.shape[3] * x.shape[4])
