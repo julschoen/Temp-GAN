3368f2d962d9ba3688fba3d83afbb14cf74fe940
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-21 10:30:12 +0200

    Update temp_gen.py

--- temp_gen.py
+++ temp_gen.py
@@ -7,7 +7,8 @@ class Generator(nn.Module):
         self.dim_z = params.z_size
         self.ngpu = params.ngpu
         self.lin = nn.Parameter(0.1 * torch.randn(self.dim_z))
+        self.lin = self.lin/torch.norm(self.lin)
 
     def forward(self, z, alpha):
-        d = torch.mul(self.lin.reshape(1,-1).transpose(0,1), alpha).transpose(0,1)
+        d = torch.mul((self.lin/torch.norm(self.lin)).reshape(1,-1).transpose(0,1), alpha).transpose(0,1)
         return z + d
