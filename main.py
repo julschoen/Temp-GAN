4ba435ca6b6058fffa1a0fbf97478947211ebc3f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-22 12:05:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--niters', type=int, default=5000, help='Number of training iterations')
-	parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
+	parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
 	parser.add_argument('--z_size', type=int, default=512, help='Latent space dimension')
 	parser.add_argument('--filterG', type=int, default=64, help='Number of filters G')
 	parser.add_argument('--filterD', type=int, default=64, help='Number of filters D')
@@ -16,7 +16,7 @@ def main():
 	parser.add_argument('--lrTempG', type=float, default=5e-5, help='Learning rate tempG')
 	parser.add_argument('--lrTempD', type=float, default=1e-4, help='Learning rate tempD')
 	parser.add_argument('--lrEnc', type=float, default=1e-5, help='Learning rate Encoder')
-	parser.add_argument('--data_path', type=str, default='../Data/4dct_clean/train_pat.npz',help='Path to data.')
+	parser.add_argument('--data_path', type=str, default='../3D-GAN/train_lidc_128.npz',help='Path to data.')
 	parser.add_argument('--ngpu', type=int, default=2, help='Number of GPUs')
 	parser.add_argument('--steps_per_log', type=int, default=10, help='Output Iterations')
 	parser.add_argument('--steps_per_img_log', type=int, default=50, help='Image Save Iterations')
