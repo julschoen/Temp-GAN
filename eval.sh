1d74cbceab6d91f04154fd66bba1312f9f1aca0d
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-29 09:13:18 +0200

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,4 +1,5 @@
 #./gan/bin/python eval_im.py --log_dir=4D_eval -l=AdvNFNorm4d -l=AdvOne4D --data_path=../Data/4dct_clean/test_pat.npz
+
 ./gan/bin/python eval_im.py --log_dir=LIDC_eval -l=AdvNFNorm -l=ClNFNorm -l=TrNFNorm --data_path=../3D-GAN/test_lidc_128.npz
 ./gan/bin/python eval_temp.py --log_dir=LIDCTemp_eval -l=AdvNFNorm -l=ClNFNorm -l=TrNFNorm
 ./gan/bin/python eval_temp.py --log_dir=4DTemp_eval -l=AdvNFNorm4d -l=AdvOne4D
