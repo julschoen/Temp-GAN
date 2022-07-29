7cd40634d0b0777c4c805f6933e78c27562ec24e
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-29 09:09:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
-./gan/bin/python main.py --log_dir=AdvNFNorm4d --norm=True --batch_size=16
-./gan/bin/python main.py --log_dir=AdvOne4D --temp_iter=2 --one_disc=True --norm=True --batch_size=16
-./gan/bin/python main.py --log_dir=AdvNFNorm4d --norm=True --batch_size=16
-./gan/bin/python main.py --log_dir=AdvOne4D --temp_iter=2 --one_disc=True --norm=True --batch_size=16
-./gan/bin/python main.py --log_dir=AdvNFNorm4d --norm=True --batch_size=16
 ./gan/bin/python main.py --log_dir=AdvOne4D --temp_iter=2 --one_disc=True --norm=True --batch_size=16
 ./gan/bin/python main.py --log_dir=AdvNF --lidc=True
 ./gan/bin/python main.py --log_dir=AdvOne --temp_iter=2 --norm=True --one_disc=True
