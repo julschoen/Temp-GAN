433340366a11cfa3a9f3363edfce4becda9664f6
julschoen <31760205+julschoen@users.noreply.github.com>
2022-07-30 12:38:52 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-./gan/bin/python main.py --log_dir=AdvTest2 --norm=True --lidc=True --niter=2000
-./gan/bin/python main.py --log_dir=TrTest2 --triplet=True --norm=True --lidc=True --niter=2000
-./gan/bin/python main.py --log_dir=ClTest2 --cl=True --norm=True --lidc=True --niter=2000
+./gan/bin/python main.py --log_dir=AdvTest3 --norm=True --lidc=True --niter=2000
+./gan/bin/python main.py --log_dir=TrTest3 --triplet=True --norm=True --lidc=True --niter=2000
+./gan/bin/python main.py --log_dir=ClTest3 --cl=True --norm=True --lidc=True --niter=2000
 #./gan/bin/python main.py --log_dir=AdvNF --lidc=True
 #./gan/bin/python main.py --log_dir=AdvOne --temp_iter=2 --norm=True --one_disc=True
