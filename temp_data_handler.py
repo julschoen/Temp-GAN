ed7121ea2f16751da6c4e7de4fbae20992476099
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-03 21:39:38 +0200

    Update temp_data_handler.py

--- temp_data_handler.py
+++ temp_data_handler.py
@@ -105,7 +105,7 @@ class Data4D():
 class DataLIDC():
   def __init__(self, path, triplet=False, shift=True):
     self.data = np.load(path)['X']
-    self.len = 600#self.data.shape[0]
+    self.len = 700#self.data.shape[0]
     self.shift = shift
     self.triplet = triplet
     self.shift_amount = np.arange(10,40)
