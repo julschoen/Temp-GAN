2f02c516e2b429de8998265ddc7fe80f5f64b4aa
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-04 10:35:07 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -454,7 +454,6 @@ class Trainer(object):
             if not self.p.one_disc:
                 for _ in range(self.p.im_iter):  
                     data, labels = next(gen)
-                    print('Got data')
                     real = data.to(self.device)
                     errImD_real, errImD_fake = self.step_imD(real[:,0])
             else:
