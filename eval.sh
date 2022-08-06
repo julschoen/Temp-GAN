a3f374043b95c311dcc6c40378bf42c5d4a4070c
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-06 12:24:29 +0200

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-./gan/bin/python eval_temp.py --log_dir=LIDCTemp_eval -l=AdvNFNormCl -l=AdvNFNormTr -l=ClNFNorm -l=TrNFNorm
+./gan/bin/python eval_temp.py --log_dir=LIDCTemp_eval -l=AdvNFNormTr -l=AdvNFNormTr -l=AdvNFNormTr -l=AdvNFNormTr
