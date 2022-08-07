dc7791816883acd7e5e79645aa70995fe56eb007
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-07 04:04:07 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-./gan/bin/python main.py --log_dir=AdvNFNormCl2 --norm=True --lidc=True
-./gan/bin/python main.py --log_dir=AdvFNorm --fixed_dir=True --norm=True --lidc=True
-./gan/bin/python main.py --log_dir=AdvF --fixed_dir=True --lidc=True
-./gan/bin/python main.py --log_dir=AdvNF --lidc=True
+#./gan/bin/python main.py --log_dir=AdvNFNormCl2 --norm=True --lidc=True
+./gan/bin/python main.py --log_dir=AdvFNorm2 --fixed_dir=True --norm=True --lidc=True
+./gan/bin/python main.py --log_dir=AdvF2 --fixed_dir=True --lidc=True
+./gan/bin/python main.py --log_dir=AdvNF2 --lidc=True
 ./run.sh
