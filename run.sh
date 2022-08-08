a6758dc593146fba262e5a8bbe3829d8e860be69
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-08 16:13:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-./gan/bin/python main.py --log_dir=AdvCBCT --norm=True --batch_size=16 --z_size=256 --cbct=True
-./gan/bin/python main.py --log_dir=Adv4D --norm=True --batch_size=16 --z_size=256
-./gan/bin/python main.py --log_dir=AdvOne --one_disc=True --norm=True --batch_size=12 --temp_iter=2 --lidc=True
+./gan/bin/python main.py --log_dir=AdvCBCT --norm=True --batch_size=16 --z_size=256 --cbct=True --niter=10000
+./gan/bin/python main.py --log_dir=Adv4D --norm=True --batch_size=16 --z_size=256 --niter=10000
+./gan/bin/python main.py --log_dir=AdvCBCT512 --norm=True --batch_size=16 --cbct=True
+./gan/bin/python main.py --log_dir=Adv4D512 --norm=True --batch_size=16
 ./run.sh
