b3af049ce42f47b64fce85db0bb4ffe8dd12870f
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-08 15:53:08 +0200

    Update eval_im.py

--- eval_im.py
+++ eval_im.py
@@ -25,8 +25,8 @@ def load_gen(path, ngpu):
 
 def eval(params):
 	#dataset = DataLIDC(path=params.data_path, triplet=True)
-	dataset = Data4D(path='../Data/4dct_clean/test_pat.npz', shift=False)
-	#dataset = DataCBCT(path='../Data/cbct/test_pat.npz', shift=False)
+	#dataset = Data4D(path='../Data/4dct_clean/test_pat.npz', shift=False)
+	dataset = DataCBCT(path='../Data/cbct/test_pat.npz', shift=False)
 	print(dataset.__len__())
 	generator = DataLoader(dataset, batch_size=params.batch_size, shuffle=True, num_workers=4)
 	os.makedirs(params.log_dir, exist_ok=True)
