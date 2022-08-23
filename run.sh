fe67f77721032c8b2839756d407317ea5ce48f1b
julschoen <31760205+julschoen@users.noreply.github.com>
2022-08-23 15:20:27 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-./gan/bin/python main.py --log_dir=AdvCBCT2 --norm=True --z_size=256 --batch_size=16 --cbct=True
-./gan/bin/python main.py --log_dir=Adv4D2 --norm=True --z_size=256 --batch_size=16
-./gan/bin/python main.py --log_dir=AdvCBCT512 --norm=True --batch_size=16 --cbct=True
-./gan/bin/python main.py --log_dir=Adv4D512 --norm=True --batch_size=16
+./gan/bin/python main.py --log_dir=Adv4DMD --norm=True --z_size=256 --batch_size=16 --md=True
+./gan/bin/python main.py --log_dir=AdvCBCTMD --norm=True --z_size=256 --batch_size=16 --cbct=True --md=True
+./gan/bin/python main.py --log_dir=AdvLIDCMD --norm=True --batch_size=8 --lidc=True --md=True
 ./run.sh
