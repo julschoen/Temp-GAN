7ec611b6c305e80409971266b2902fa66232c0e5
julschoen <31760205+julschoen@users.noreply.github.com>
2022-12-07 17:18:34 +0100

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-./gan/bin/python eval_temp.py --log_dir=FinalTemp_eval -l=Adv4D -l=AdvCBCT
+./gan/bin/python eval_im.py --log_dir=FinalTemp_eval -l=Adv4D --data_path='../Data/4dct_clean/test_pat.npz' 
