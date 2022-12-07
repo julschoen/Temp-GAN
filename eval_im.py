30a7f03df2243640372c4acc4330beb926c0a4bd
julschoen <31760205+julschoen@users.noreply.github.com>
2022-12-07 17:47:46 +0100

    Update eval_im.py

--- eval_im.py
+++ eval_im.py
@@ -44,7 +44,7 @@ def eval(params):
 		large_fake = None
 		with torch.no_grad():
 			for i, (data, _) in enumerate(generator):
-				x1 = data.unsqueeze(1)
+				#x1 = data.unsqueeze(1)
 				x1 = x1[:,np.random.choice([0,1,2])].unsqueeze(1)
 				if params.ngpu > 1:
 					noise = torch.randn(x1.shape[0], netG.module.dim_z, dtype=torch.float, device=params.device)
