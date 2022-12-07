03ad77bea7c52f1edb0a1ed80e14d8ebeee3ea0a
julschoen <31760205+julschoen@users.noreply.github.com>
2022-12-07 19:05:33 +0100

    Update eval_im.py

--- eval_im.py
+++ eval_im.py
@@ -28,9 +28,9 @@ def load_gen(path, ngpu):
 	return netG.to(params.device), tempG.to(params.device)
 
 def eval(params):
-	#dataset = DataLIDC(path='../3D-GAN/test_lidc_128.npz', triplet=True)
+	dataset = DataLIDC(path='../3D-GAN/test_lidc_128.npz', triplet=True)
 	#dataset = Data4D(path='../Data/4dct_clean/test_pat.npz', shift=True)
-	dataset = DataCBCT(path='../Data/cbct/test_pat.npz', shift=True)
+	#dataset = DataCBCT(path='../Data/cbct/test_pat.npz', shift=True)
 	print(dataset.__len__())
 	generator = DataLoader(dataset, batch_size=params.batch_size, shuffle=True, num_workers=4)
 	os.makedirs(params.log_dir, exist_ok=True)
